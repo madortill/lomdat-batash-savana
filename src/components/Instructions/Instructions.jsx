f970f537b5adbd18629e1343c762087648794f50
madortill <mador.till@gmail.com>
2026-04-26 18:28:23 +0300

    fourth upload

--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -15,14 +15,17 @@ const Instructions = ({ onSendData }) => {
     return (
         <div className="instructions-page">
             {/* <h1>{Instructions}</h1> */}
-            <img src={HappyGal} className={styles.instructionsGal} alt="happy-gal"/>
+            <p className="main-header-text">This is lomdat Savana!</p>
+            <img src={HappyGal} className={styles.instructionsGal} alt="happy-gal" />
             {step === 0 &&
                 <>
-                    <p>This is lomdat Savana!</p>
                     <div className={styles.textBox}>
                         <p className={styles.bubbleText}>hi i am bubble text</p>
                     </div>
                 </>}
+            <div className={`next-btn ${styles.nextBtn}`}>
+                <p className="next-btn-text">NEXT</p>
+            </div>
         </div >
     );
 };
