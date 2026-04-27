d0a525ff562a6e15afed6b3116866ea5267e7b63
madortill <mador.till@gmail.com>
2026-04-27 15:39:26 +0300

    sixth upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Instructions from "./components/Instructions/Instructions";
 import "./App.css";
-import backBtn from "./assets/img/backBtn.svg";
 
 function App() {
 
   return (
     <>
       <div className="app">
-        <img src={backBtn} className="back-btn"/>
         <Routes>
           <Route path="/" element={<Instructions />} />
           {/* <Route path="/intro" element={<Opening />} /> */}
+          {/* <Route path="/intro" element={<NavPage />} /> */}
           {/* <Route path="/home" element={<Content />} /> */}
           {/* <Route path="/end" element={<End />} /> */}
         </Routes>
--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -3,27 +3,58 @@ import { useNavigate } from "react-router-dom";
 import { useData } from "../../context/DataContext";
 import styles from "./Instructions.module.css";
 import HappyGal from "../../assets/img/HappyGal.svg";
+import backBtn from "../../assets/img/backBtn.svg";
 
 
 const Instructions = ({ onSendData }) => {
 
     // const { data } = useData();
     // const Instructions = data.Instructions[0].text;
-
+    const navigate = useNavigate();
     const [step, setStep] = useState(0);
+    const TOTAL_STEPS = 2;
+
+    const handleNext = () => {
+        if (step < TOTAL_STEPS) {
+            setStep(prev => prev + 1);
+        } else {
+            navigate("/NavPage");
+        }
+    };
+
+    const handleBack = () => {
+        if (step > 0) {
+            setStep(prev => prev - 1);
+        } else {
+            navigate("/");
+        }
+    };
 
     return (
         <div className="instructions-page">
+            <img src={backBtn} className="back-btn" onClick={handleBack}/>
             {/* <h1>{Instructions}</h1> */}
             <p className="main-header-text">This is lomdat Savana!</p>
             <img src={HappyGal} className={styles.instructionsGal} alt="happy-gal" />
             {step === 0 &&
                 <>
                     <div className={styles.textBox}>
-                        <p className={styles.bubbleText}>hi i am bubble text</p>
+                        <p className={styles.bubbleText}>hi i am bubble text Language</p>
                     </div>
                 </>}
-            <div className={`next-btn ${styles.nextBtn}`}>
+            {step === 1 &&
+                <>
+                    <div className={styles.textBox}>
+                        <p className={styles.bubbleText}>hi i am bubble text sound</p>
+                    </div>
+                </>}
+            {step === 2 &&
+                <>
+                    <div className={styles.textBox}>
+                        <p className={styles.bubbleText}>hi i am bubble text buttons</p>
+                    </div>
+                </>}
+            <div className={`next-btn ${styles.nextBtn}`} onClick={handleNext} >
                 <p className="next-btn-text">NEXT</p>
             </div>
         </div >
